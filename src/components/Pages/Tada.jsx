6e514fc1abfed16e7049662e3ba0c3c354fd6682
BoteritoROOT <d.sajor2003rp@gmail.com>
2024-12-19 19:01:56 -0400

    ah ya

--- src/components/Pages/Tada.jsx
+++ src/components/Pages/Tada.jsx
@@ -11,20 +11,20 @@ const Tada = () => {
     }
 
     return (
+
         <div className='container'>
 
-            <div className="magia">
-                <h1>PRECIOS</h1>
-                <h1>MAGICOS</h1>
-            </div>
 
             <div className='Tada'>
+                <div className="magia">
+                    <h1>PRECIOS</h1>
+                    <h1>MAGICOS</h1>
+                </div>
                 <button onClick={navegation}>DESCUENTOS DE CERVEZAS</button>
                 <button onClick={navegation}>COMBOS</button>
                 <button onClick={navegation}>PROMOCIONES</button>
-            </div>
-            <div>
-                <img src="/img/TADA.png" alt="" />
+
+                    <img src="/img/TADA.png" alt="" />
             </div>
         </div>
     )
