251e8f95ff1e5a118339d7b4d23a5127ef9bea48
BoteritoROOT <d.sajor2003rp@gmail.com>
2024-12-19 17:22:40 -0400

    probando

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Tada from './components/Pages/Tada';
 import PreciosMagicos from './components/Pages/PreciosMagicos';
+import { Navbar } from './components/Navar/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
 function Main() {
   return (
     <>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Tada/>} />
         <Route path="/preciosMagicos" element={<PreciosMagicos/>} />
--- src/components/Pages/Tada.jsx
+++ src/components/Pages/Tada.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../css/Tada.css'
 import { useNavigate } from 'react-router-dom'
 
 const Tada = () => {
@@ -9,12 +10,24 @@ const Tada = () => {
         navigate("/preciosMagicos")
     }
 
-  return (
-    <div>
-      hola tada cadabra
-      <button onClick={navegation}>hola</button>
-    </div>
-  )
+    return (
+        <div className='container'>
+
+            <div className="magia">
+                <h1>PRECIOS</h1>
+                <h1>MAGICOS</h1>
+            </div>
+
+            <div className='Tada'>
+                <button onClick={navegation}>DESCUENTOS DE CERVEZAS</button>
+                <button onClick={navegation}>COMBOS</button>
+                <button onClick={navegation}>PROMOCIONES</button>
+            </div>
+            <div>
+                <img src="/img/TADA.png" alt="" />
+            </div>
+        </div>
+    )
 }
 
 export default Tada
